dec7625725ea97554255b96c6ae9e2f72ce9552a
Kenneth Brook <kenneth.brook@hotmail.com>
2023-10-06 17:20:41 -0400

    need to fix cors

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+const clientList = document.getElementById('clients')
+
 function pageLaunch() {
-    console.log('Connected');
+    fetch('https://ens.a2hosted.com/client-control-host/clients') 
+  .then(response => { 
+    if (response.ok) { 
+      return response.json(); // Parse the response data as JSON 
+    } else { 
+      throw new Error('API request failed'); 
+    } 
+  }) 
+  .then(data => { 
+    // Process the response data here 
+    console.log(data); // Example: Logging the data to the console 
+  }) 
+  .catch(error => { 
+    // Handle any errors here 
+    console.error(error); // Example: Logging the error to the console 
+  });
 }
