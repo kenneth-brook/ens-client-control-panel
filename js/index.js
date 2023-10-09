01eac6b8a1d5fcea9f71d30d22e2f861892310bc
Kenneth Brook <kenneth.brook@hotmail.com>
2023-10-09 12:38:04 -0400

    client select done, single client get started

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const clientList = document.getElementById('clients')
+const clientList = document.getElementById('clients');
+let singleGrab = "";
 
 function pageLaunch() {
     fetch('https://ens.a2hosted.com/client-control-host/clients') 
@@ -9,9 +10,32 @@ function pageLaunch() {
       throw new Error('API request failed'); 
     } 
   }) 
-  .then(data => { 
-    // Process the response data here 
-    console.log(data); // Example: Logging the data to the console 
+  .then(data => {
+    data.forEach((client) => {
+        let id = client.id;
+        let name = client.name;
+        let opItem = `<option value=${id}>${name}</option>`;
+        console.log(opItem)
+        clientList.innerHTML += opItem;
+    });
+  }) 
+  .catch(error => { 
+    // Handle any errors here 
+    console.error(error); // Example: Logging the error to the console 
+  });
+}
+
+function selectClient() {
+    fetch(`https://ens.a2hosted.com/client-control-host/clients/`) 
+  .then(response => { 
+    if (response.ok) { 
+      return response.json(); // Parse the response data as JSON 
+    } else { 
+      throw new Error('API request failed'); 
+    } 
+  }) 
+  .then(data => {
+    
   }) 
   .catch(error => { 
     // Handle any errors here 
